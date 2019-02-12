008aae44f19d08887225ec7621b6b042cd729d88
Spencer Davis <spencerfdavis@gmail.com>
2019-02-12 10:28:26 -0700

    Add tests

--- test/errors.js
+++ test/errors.js
@@ -11,6 +11,19 @@ describe('errors', () => {
     res(undefined)
   })
 
+  describe('non JSON formatted error', () => {
+    beforeEach(() =>
+      gimme({ url: `${url}/non-json-error` }).catch(res)
+    )
+
+    it('rejects with an appropriate Boom error', () => {
+      expect(res()).to.be.a('Error')
+      expect(res().isBoom).to.be.true
+      expect(res().output.statusCode).to.equal(400)
+      expect(res().data.res.body).to.equal('string error')
+    })
+  })
+
   describe('when statusCode >= 400', () => {
     beforeEach(() =>
       gimme({ url: `${url}/error` }).catch(res)
